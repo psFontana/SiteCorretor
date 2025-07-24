8c53a838e62db8e16a0c218afa10bfa60f45439e
psFontana <166160927+psFontana@users.noreply.github.com>
2025-07-24 16:23:39 -0300

    .

--- script.js
+++ script.js
@@ -1,54 +1,103 @@
-// Carregar imóveis de imoveis.json
-fetch("imoveis.json")
-  .then((res) => res.json())
-  .then((imoveis) => {
-    const carouselContent = document.getElementById("carousel-content");
+let fullpageInstance = null;
+const mobileBreakpoint = 768;
 
-    // Preencher o carrossel
-    imoveis.forEach((imovel, index) => {
-      const item = `
-        <div class="carousel-item ${index === 0 ? "active" : ""}">
-          <img src="${imovel.imagem}" class="d-block" alt="${imovel.titulo}">
-        </div>
-      `;
-      carouselContent.innerHTML += item;
-    });
-  });
-// if (window.innerWidth > 768) {
-//   new fullpage("#fullpage", {
-//     scrollBar: true,
-//     navigation: true,
-//     anchors: ["home", "sobre", "contato"],
-//     menu: false,
-//     afterLoad: function (origin, destination, direction) {
-//       if (destination.index === 0) {
-//         $(".header-links i").css("color", "white");
-//       } else {
-//         $(".header-links i").css("color", "black");
-//       }
-//     },
-//   });
-// } else {
-//   console.log("FullPage não ativado (mobile)");
-// }
+function handleFullPageLoad() {
+  if (window.innerWidth > mobileBreakpoint) {
+    if (!fullpageInstance) {
+      let fullpageScript = document.createElement("script");
+      fullpageScript.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.14/fullpage.min.js";
+      fullpageScript.onload = function () {
+        fullpageInstance = new fullpage("#fullpage", {
+          licenseKey: "YOUR_LICENSE_KEY_HERE",
+          scrollBar: true,
+          navigation: true,
+          anchors: ["home", "sobre", "contato"],
+          menu: false,
+          afterLoad: function (origin, destination, direction) {
+            if (destination.index === 0) {
+              $(".header-links i").css("color", "white");
+            } else {
+              $(".header-links i").css("color", "black");
+            }
+          },
+        });
 
-if (window.innerWidth > 768) {
-  let essabosta = document.createElement("script");
-  essabosta.src =
-    "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/4.0.14/fullpage.min.js";
-  document.head.appendChild(essabosta);
+        $(".nav-links a")
+          .off("click")
+          .on("click", function (e) {
+            e.preventDefault();
+            if (fullpageInstance) {
+              const anchor = $(this).attr("href").substring(1);
+              fullpageInstance.silentMoveTo(anchor);
+            }
+            $(".nav-screen").css("right", "-285px");
+            $("body").css("right", "0");
+          });
+      };
+      document.head.appendChild(fullpageScript);
+    }
+  } else {
+    if (fullpageInstance) {
+      fullpageInstance.destroy("all");
+      fullpageInstance = null;
+
+      $('a[href*="#"]')
+        .not('[href="#"]')
+        .off("click")
+        .on("click", function (e) {
+          if (
+            location.pathname.replace(/^\//, "") ===
+              this.pathname.replace(/^\//, "") &&
+            location.hostname === this.hostname
+          ) {
+            var target = $(this.hash);
+            target = target.length
+              ? target
+              : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+              $("html, body").animate({ scrollTop: target.offset().top }, 700);
+              return false;
+            }
+          }
+        });
+    }
+  }
 }
 
+handleFullPageLoad();
+
+let resizeTimeout;
+window.addEventListener("resize", function () {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(handleFullPageLoad, 200);
+});
+
 $(document).ready(function () {
-  // 1) typing animation
+  fetch("imoveis.json")
+    .then((res) => res.json())
+    .then((imoveis) => {
+      const carouselContent = document.getElementById("carousel-content");
+      imoveis.forEach((imovel, index) => {
+        const item = `
+          <div class="carousel-item ${index === 0 ? "active" : ""}">
+            <img src="${imovel.imagem}" class="d-block" alt="${imovel.titulo}">
+          </div>
+        `;
+        carouselContent.innerHTML += item;
+      });
+    });
+
   (function ($) {
     $.fn.writeText = function (content) {
       var contentArray = content.split(""),
         current = 0,
         elem = this;
-      setInterval(function () {
+      var intervalId = setInterval(function () {
         if (current < contentArray.length) {
           elem.text(elem.text() + contentArray[current++]);
+        } else {
+          clearInterval(intervalId);
         }
       }, 80);
     };
@@ -56,10 +105,8 @@ $(document).ready(function () {
 
   $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
 
-  // 2) wow.js
   new WOW().init();
 
-  // 3) nav-screen toggle
   $(".menu-toggle").click(function () {
     $(".nav-screen").css("right", "0");
     $("body").css("right", "285px");
@@ -70,43 +117,6 @@ $(document).ready(function () {
     $("body").css("right", "0");
   });
 
-  // 4) fullPage.js
-  new fullpage("#fullpage", {
-    scrollBar: true,
-    navigation: true,
-    anchors: ["home", "sobre", "contato"],
-    menu: false,
-    afterLoad: function (origin, destination, direction) {
-      // muda estilo de header-links conforme seção
-      if (destination.index === 0) {
-        $(".header-links i").css("color", "white");
-      } else {
-        $(".header-links i").css("color", "black");
-      }
-    },
-  });
-
-  // 5) smooth anchors (caso use links normais)
-  $('a[href*="#"]')
-    .not('[href="#"]')
-    .click(function () {
-      if (
-        location.pathname.replace(/^\//, "") ===
-          this.pathname.replace(/^\//, "") &&
-        location.hostname === this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html, body").animate({ scrollTop: target.offset().top }, 700);
-          return false;
-        }
-      }
-    });
-
-  // 6) AJAX form
   var form = $("#ajax-contact");
   var formMessages = $("#form-messages");
 
@@ -130,11 +140,3 @@ $(document).ready(function () {
       });
   });
 });
-
-// Remover watermark do fullPage.js
-// setTimeout(() => {
-//   const watermark = document.querySelector(".fp-watermark");
-//   if (watermark) {
-//     watermark.remove();
-//   }
-// }, 100);
