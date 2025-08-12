cc30bfc50f2fb24c0cfe1d85d16994a941cd061a
psFontana <166160927+psFontana@users.noreply.github.com>
2025-08-12 00:56:31 -0300

    Update crud.js

--- functions/crud.js
+++ functions/crud.js
@@ -1,6 +1,13 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
 app.get("/", (_req, res) => {
   res.json({
     ok: true,
     auth0Audience: process.env.AUTH0_AUDIENCE || "não definida",
   });
 });
+
+module.exports.handler = serverless(app);
